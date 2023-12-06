2b7a9c9a64bd9464369048757f2f3f2e2502b8fc
chrvstopher19 <140680259+chrvstopher19@users.noreply.github.com>
2023-12-06 17:59:44 -0500

    Added routes in app.js and worked on the feed.jsx

--- src/screens/Feed.jsx
+++ src/screens/Feed.jsx
@@ -1,33 +1,33 @@
-// import React from 'react'
-// import {useEffect, useState} from 'react'
-// // import {getDishes} from '../services/dishes.js'
-// import Post from '../components/Post.jsx'
+import React from 'react'
+import {useEffect, useState} from 'react'
+import {getPosts} from '../services/posts.js'
+import Post from '../components/Post.jsx'
 
-// function Posts( //{setShowNav}
-// ) {
-// //   setShowNav(true)
-//     const [posts , setPosts] = useState([])
+function Posts( //{setShowNav}
+) {
+//   setShowNav(true)
+    const [posts , setPosts] = useState([])
 
-//     useEffect(()=>{
-//       fetchPosts()
-//     }, []) //only fire this function one time 
+    useEffect(()=>{
+      fetchPosts()
+    }, []) //only fire this function one time 
   
-//     async function fetchPosts () {
-//       const allPosts = await getPosts()
-//       setPosts(allPosts)
-//     }
+    async function fetchPosts () {
+      const allPosts = await getPosts()
+      setPosts(allPosts)
+    }
   
-//     return (
-//       <div>
-//         <h1>Post</h1>
-//         <div className="post-container">
-//           {
-//             posts.map((post)=>(
-//               <Post post={post} />
-//             ))
-//           }
-//         </div>
-//       </div>
-//     )
-//   }
-// export default Post
+    return (
+      <div>
+        <h1>Post</h1>
+        <div className="post-container">
+          {
+            posts.map((post)=>(
+              <Post post={post} />
+            ))
+          }
+        </div>
+      </div>
+    )
+  }
+export default Post
