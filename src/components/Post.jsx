afd6e4f3ec13cbe0db2863a98443ee1c442b898e
manasvislj <manasvisaluja2003@gmail.com>
2023-12-07 17:53:37 -0500

    pst finished

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
+import { HiLocationMarker } from "react-icons/hi";
 // import {Link} from 'react-router-dom'
 // import "../css/Characters.css"
 
 function Post({post}) {
-  console.log(post)
   return (
     <div className="bg-white border-2 border-gray-300 rounded-3xl shadow-md max-w-md mx-auto my-4">
     {/* Header - User Info */}
     <div className="flex items-center p-4">
         <img
           className="h-10 w-10 rounded-full object-cover"
-          src={post.image_url}
+          src={post.user_profile.profile_pic_url}
           // alt={post.details}
         />
-        <span className="ml-2 font-semibold">{post.user_profile}</span>
+        <span className="ml-2 font-semibold">{post.user_profile.user.username}</span>
     </div>
     {/* Image */}
         <img
@@ -21,14 +21,14 @@ function Post({post}) {
               src={post.image_url}
               alt={post.details}
             />
-
       {/* Caption */}
-      <div className="px-4 pb-2 flex justify-between p-4">
-        <span className="font-semibold">{post.location}</span>
+      <div className="px-4 pb-2 flex justify p-4">
+        <span className="flex items-center text-red-700 text-2xl"><HiLocationMarker /></span>
+        <span className="flex items-center font-semibold">&nbsp;{post.location}</span>
       </div>
 
       <div className="px-4 pb-2 ">
-        <span className="font-semibold">{post.user_profile} </span>{post.details}
+        <span className="font-semibold">{post.user_profile.user.username} </span>{post.details}
       </div>
 
     </div>
