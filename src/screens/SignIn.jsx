abaae03470b8e674e7ecd92c919815bbbd326150
Sara Mejia <sage@Saras-MacBook-Air.local>
2023-12-11 16:10:31 -0500

    fixed error

--- src/screens/SignIn.jsx
+++ src/screens/SignIn.jsx
@@ -73,8 +73,8 @@ const SignIn = (props) => {
   const { username, password } = form
 
   return (
-    <div className="bg-cover bg-center" style={{ backgroundImage: "url('https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')" }}>
-      <div className="flex items-center justify-center min-h-screen absolute top-0 left-0 w-full">
+    <div className="relative h-screen">
+      <div className="flex items-center justify-center min-h-screen absolute top-0 left-0 w-full bg-cover bg-center" style={{ backgroundImage: "url('https://images.unsplash.com/photo-1540189549336-e6e99c3679fe?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')" }}>
         <section className="z-10">
           <div className="flex flex-col items-center justify-center  bg-gray-800 px-6 py-8 mx-auto md:h-screen lg:py-0 ">
             <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-gray-900">
@@ -105,7 +105,7 @@ const SignIn = (props) => {
                       className="bg-gray-50 border border-gray-300 text-white text-center sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       required=""
                       onChange={handleChange}
-                      />
+                    />
                   </div>
                   {renderError()}
                   <p className="text-sm font-medium text-white">
@@ -114,9 +114,8 @@ const SignIn = (props) => {
                   <button
                     type="button"
                     className="w-full text-white bg-gray-500 hover:bg-gray-600 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mt-2 dark:bg-gray-500 dark:hover:bg-gray-600 dark:focus:ring-gray-800"
-                  ><Link to="/feed">
-                    Nosey?
-                    </Link>
+                  >
+                    <Link to="/feed">Nosey?</Link>
                   </button>
                 </form>
               </div>
@@ -126,18 +125,6 @@ const SignIn = (props) => {
       </div>
     </div>
   );
-};
+}
 
 export default SignIn;
-
-{/* <Slider {...settings} className="absolute top-0 left-0 w-full h-ful">
-        {images.map((image, index) => (
-          <div key={index} className="w-full h-full">
-            <img
-              src={image}
-              alt={`Slider Image ${index + 1}`}
-              className="w-full h-full object-cover"
-            />
-          </div>
-        ))}
-      </Slider> */}
