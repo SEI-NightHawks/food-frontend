62b09207c05367e5a9c834367f3f6f26c3576623
manasvislj <manasvisaluja2003@gmail.com>
2023-12-11 11:46:00 -0500

    Navbar , SignIn, couple other things

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import Feed from '../src/screens/Feed.jsx'
 import Profile from "../src/screens/Profile.jsx"
 import SignUp from "../src/screens/SignUp.jsx"
-import SignIn from "../src/screens/SignIn.jsx"
+import SignIn from "./screens/SignIn.jsx"
 import getPosts from "./services/posts.js"
 import { Route, Routes } from 'react-router-dom';
 import Nav from "./components/Nav.jsx"
@@ -12,15 +12,16 @@ import { isAuthenticated } from '../src/services/authUtils.js';
 
 
 function App () {
+  const [user, setUser] = useState(null);
 
   return (
     <div className='App'>
       <Routes>
         {/* <Route path="/profile/:profileId" element={<Profile/>}/>  */}
         <Route path="/feed" element={<Feed/> }/>
-        <Route path="/sign-up" element={<SignUp/> }/>
-        <Route path="/" element={<SignIn/>}/>
-        <Route path="/sign-in" element={<SignIn/> }/>
+        <Route path="/sign-up" element={<SignUp setUser={setUser}/> }/>
+        <Route path="/" element={<SignIn setUser={setUser}/>}/>
+        <Route path="/sign-in" element={<SignIn setUser={setUser}/> }/>
       </Routes>
     </div>
   );
